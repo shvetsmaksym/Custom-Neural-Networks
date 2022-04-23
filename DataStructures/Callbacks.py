fix for multiclass error calculation

DataStructures/Callbacks.py
@@ -16,19 +16,21 @@ class Metrics:
         self.metrics_to_use = {metric: self.metrics_functions[metric] for metric in metrics_list}
         self.neural_net.metric_history = {m: [] for m in metrics_list}
 
-    def calculate_metrics(self):
+    def calculate_metrics(self, verbose=0):
         for func in self.metrics_to_use.values():
-            func()
+            func(verbose)
 
-    def accuracy(self):
+    def accuracy(self, verbose=0):
         res = np.average(self.neural_net.basic_metrics_history['correct_classifications'][-1])
         self.neural_net.metric_history['acc'].append(res)
-        print(f"Accuracy: {round(res, 4)}", end="\t")
+        if verbose:
+            print(f"Accuracy: {round(res, 4)}", end="\t")
 
-    def MSE(self):
+    def MSE(self, verbose=0):
         res = 1/2 * (np.average(self.neural_net.basic_metrics_history['errors'][-1]) ** 2)
         self.neural_net.metric_history['mse'].append(res)
-        print(f"MSE: {round(res,4)}", end="\t")
+        if verbose:
+            print(f"MSE: {round(res,4)}", end="\t")
 
 
 class EarlyStopping:

DataStructures/NeuralNetwork.py
@@ -83,7 +83,7 @@ class NeuralNetwork:
 
         if validation_data:
             for i, xi in enumerate(validation_data[0]):
-                yi = np.array([validation_data[1][i]])
+                yi = np.array([validation_data[1][i]]).flatten()
                 self.feed_forward(xi)
 
                 errors[i] = self.output_layer.calculate_error(Y=yi)
@@ -91,11 +91,13 @@ class NeuralNetwork:
 
         return errors, correct_classifications
 
-    def calculate_metrics(self):
-        print(f"\nEpoch {len(self.basic_metrics_history['errors'])}:", end='\t')
-        err = self.basic_metrics_history['errors'][-1]
-        corr_cl = self.basic_metrics_history['correct_classifications'][-1]
-        self.metrics.calculate_metrics()
+    def calculate_metrics(self, epoch, freq=10):
+        if epoch % freq == 0:
+            print(f"\nEpoch {epoch}...", end='\t')
+            verbose = 1
+        else:
+            verbose = 0
+        self.metrics.calculate_metrics(verbose=verbose)
 
     def perform_one_epoch(self, train_x, train_y, lr):
         for xi, yi in zip(train_x, train_y):
@@ -121,7 +123,7 @@ class NeuralNetwork:
             err, corr_cl = self.calculate_errors(validation_data=validation_data)
             self.basic_metrics_history["errors"].append(err)
             self.basic_metrics_history["correct_classifications"].append(corr_cl)
-            self.calculate_metrics()
+            self.calculate_metrics(epoch)
 
             # Check for early stopping
             if self.early_stopping is not None: